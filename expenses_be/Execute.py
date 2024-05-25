bdb32d4fde26c95d2b28dc4ea06d7af774089b1a
dcjh <140426836+dcjh@users.noreply.github.com>
2024-05-25 18:51:41 +0800

    updated expenses_be

---
+++ expenses_be/Execute.py
@@ -0,0 +1,3 @@
+from ExpenseOverview import Expense
+from MarginCalculation import *
+
---
+++ expenses_be/dataGeneration.py
@@ -0,0 +1,6 @@
+from firebase_read import *
+from expenses_be.firebase_insert import *
+
+
+doc = FirebaseRead("overview")
+print(doc)
